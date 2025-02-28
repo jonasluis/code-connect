b11c9e1786a9445c70cbd646e28c8b5ccee0cf39
jonasluis <jonasluis66@gmail.com>
2025-02-28 15:41:28 -0300

    feat: simulação de envio do formulario

--- scripts.js
+++ scripts.js
@@ -80,3 +80,37 @@ inputTags.addEventListener("keypress", async (evento) => {
         }
     }
 })
+
+const botaoPublicar = document.querySelector(".botao-publicar")
+
+async function publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjetos){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const deuCerto = Math.random() > 0.5
+
+            if(deuCerto) {
+                resolve("Projeto publicado com sucesso.")
+            }else {
+                reject("Erro ao publicar o projeto.")
+            }
+        }, 2000)
+    })
+}
+
+botaoPublicar.addEventListener("click", async (evento) => {
+    evento.preventDefault()
+
+    const nomeDoProjeto = document.getElementById("nome").value
+    const descricaoDoProjeto = document.getElementById("descricao").value
+    const tagsProjetos = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent)
+
+    try{
+        const resultado = await publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjetos)
+        console.log(resultado)
+        alert("Deu tudo certo!")
+    }catch(error){
+        console.log("Deu errado: ", error)
+        alert("Deu tudo Errado.")
+    }
+
+})
