038cf0e2fc94a15a01da62e5c78facc42d3fc60a
jonasluis <jonasluis66@gmail.com>
2025-02-28 15:04:41 -0300

    feat:  Definir tag possiveis, Limitar tags inseridas

--- scripts.js
+++ scripts.js
@@ -41,22 +41,42 @@ const inputTags = document.getElementById("input-tags")
 
 const listaTags = document.getElementById("lista-tags")
 
-inputTags.addEventListener("keypress", (evento) => {
-    if (evento.key === "Enter"){
-        evento.preventDefault()
-        const tagTexto = inputTags.value.trim()
-        if(tagTexto !== "") {
-            const tagNova = document.createElement("li")
-            tagNova.innerHTML =`<p>${tagTexto}</p> <img src="./img/close-black.svg" class="remove-tag">`
-            listaTags.appendChild(tagNova)
-            inputTags.value = ""
-        }
-    }
-})
-
 listaTags.addEventListener("click", (evento) =>{
     if (evento.target.classList.contains("remove-tag")) {
         const tagRemover = evento.target.parentElement
         listaTags.removeChild(tagRemover)
+    }
+})
+
+const tagsDisponiveis = ["Front-end", "Back-end", "Full-stack", "Devops", "CyberSecurity", "Data Science", "Mobile"]
+
+async function verificaTagsDisponiveis(tagTexto){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(tagsDisponiveis.includes(tagTexto))
+        }, 1000)
+    })
+}
+
+inputTags.addEventListener("keypress", async (evento) => {
+    if (evento.key === "Enter"){
+        evento.preventDefault()
+        const tagTexto = inputTags.value.trim()
+        if(tagTexto !== "") {
+            try{
+                const tagExiste = await verificaTagsDisponiveis(tagTexto)
+                if(tagExiste){
+                    const tagNova = document.createElement("li")
+                    tagNova.innerHTML =`<p>${tagTexto}</p> <img src="./img/close-black.svg" class="remove-tag">`
+                    listaTags.appendChild(tagNova)
+                    inputTags.value = ""
+                } else {
+                    alert("Tag não foi encontrada.")
+                }
+            } catch (error){
+                console.log("Erro ao verificar a existencia da tag")
+                alert("Erro ao verificar a existencia da tag. Verifique o console.")
+            }
+        }
     }
 })
